efd83ec8886c27f293f803e8c7807caaae955b5a
Roozbeh Maheronnaghsh <jahesh.automation@gmail.com>
2023-08-28 22:00:44 +0330

    fixing PyPDF2

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 
 from dotenv import load_dotenv
 import streamlit as st
-from pypdf2 import PdfReader
+from PyPDF2 import PdfReader
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title='Ask your PDF')
-    st.header('Ask your PDF!!!')
+    st.header("Ask your PDF 💬")
 
     # upload file
     pdf = st.file_uploader('Upload your PDF', type='pdf')
