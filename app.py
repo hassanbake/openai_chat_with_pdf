a67743397a47a4073ef4500798cb00f0fe50ee40
Maherolnaghsh Rouzbeh <R.Maherolnaghsh@etadbir.com>
2023-08-29 11:04:38 +0330

    Creat embeddings and knowledge base

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def main():
@@ -31,6 +33,12 @@ def main():
         chunks = text_splitter.split_text(text)
         
         st.write(chunks)
+
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        
+
 
 if __name__=='__main__':
     main()
